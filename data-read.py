ba213c2b9eebd101a58c09eeb132629968a28da8
Krzysztof Kajkowski <cayco@cayco.pl>
2020-08-13 16:11:00 +0100

    Added Kettle support

--- data-read.py
+++ data-read.py
@@ -10,6 +10,21 @@ import configparser
 import os
 import json
 import datetime
+import argparse
+import re
+import sys
+import logging
+import time
+
+from mikettle.mikettle import MiKettle
+from mikettle.mikettle import (
+  MI_ACTION,
+  MI_MODE,
+  MI_SET_TEMPERATURE,
+  MI_CURRENT_TEMPERATURE,
+  MI_KW_TYPE,
+  MI_KW_TIME
+)
 
 workdir = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
@@ -22,94 +37,99 @@ averages = configparser.ConfigParser()
 averages.read("{0}/averages.ini".format(workdir))
 
 messages = []
+def valid_mikettle_mac(mac, pat=re.compile(r"[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}")):
+    """Check for valid mac adresses."""
+    if not pat.match(mac.upper()):
+        raise argparse.ArgumentTypeError('The MAC address "{}" seems to be in the wrong format'.format(mac))
+    return mac
 
-for device in devices:
 
-    mac = config[device].get("device_mac")
-    poller = MiTempBtPoller(mac, BluepyBackend, ble_timeout=config[device].getint("timeout", 10))
+def valid_product_id(product_id):
+    try:
+        product_id = int(product_id)
+    except Exception:
+        raise argparse.ArgumentTypeError('The Product Id "{}" seems to be in the wrong format'.format(product_id))
+    return product_id
+
+def kettle_connect(args):
+    """Connect to Mi Kettle."""
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    kettle = MiKettle(args.get("device_mac"), int(args.get("product_id")))
+    print("Authenticating")
+    print("Getting data from mi Kettle")
 
     try:
+      print("Current temperature: {}".format(kettle.parameter_value(MI_CURRENT_TEMPERATURE)))
+    except Exception as error:
+      print("Read failed")
+      print(error)
+    return kettle
 
-        temperature = poller.parameter_value(MI_TEMPERATURE)
-        humidity = poller.parameter_value(MI_HUMIDITY)
-        battery = poller.parameter_value(MI_BATTERY)
+def read_devices():
+        for device in devices:
+            mac = config[device].get("device_mac")
+            if config[device].get("device_type") != "mikettle":
+                poller = MiTempBtPoller(mac, BluepyBackend, ble_timeout=config[device].getint("timeout", 10))
+                try:
+                    temperature = poller.parameter_value(MI_TEMPERATURE)
+                    humidity = poller.parameter_value(MI_HUMIDITY)
+                    battery = poller.parameter_value(MI_BATTERY)
+                
+                    data = json.dumps({
+                        "temperature": temperature,
+                        "humidity": humidity,
+                        "battery": battery
+                    })
+                
+                    print(datetime.datetime.now(), device, " : ", data)
+                    messages.append({'topic': config[device].get("topic"), 'payload': data, 'retain': config[device].getboolean("retain", False)})
+                    availability = 'online'
+                except BTLEException as e:
+                    availability = 'offline'
+                    print(datetime.datetime.now(), "Error connecting to device {0}: {1}".format(device, str(e)))
+                except Exception as e:
+                    availability = 'offline'
+                    print(datetime.datetime.now(), "Error polling device {0}. Device might be unreachable or offline.".format(device))
+                    # print(traceback.print_exc())
+                finally:
+                    messages.append({'topic': config[device].get("availability_topic"), 'payload': availability, 'retain': config[device].getboolean("retain", False)})
+            else:
+                kettle = kettle_connect(config[device])
+                data = json.dumps({
+                    "current_temperature": kettle.parameter_value(MI_CURRENT_TEMPERATURE),
+                    "set_temperature": kettle.parameter_value(MI_SET_TEMPERATURE),
+                    "action": kettle.parameter_value(MI_ACTION),
+                    "mode": kettle.parameter_value(MI_MODE),
+                    "warm_type": kettle.parameter_value(MI_KW_TYPE)
+                })
+                messages.append({'topic': config[device].get("topic"), 'payload': data, 'retain': config[device].getboolean("retain", False)})
+            sendMQTT()
+def sendMQTT(): 
+        # Init MQTT
+        mqtt_config = configparser.ConfigParser()
+        mqtt_config.read("{0}/mqtt.ini".format(workdir))
+        mqtt_broker_cfg = mqtt_config["broker"]
+        
+        try:
+            auth = None
+            mqtt_username = mqtt_broker_cfg.get("username")
+            mqtt_password = mqtt_broker_cfg.get("password")
+        
+            if mqtt_username:
+                auth = {"username": mqtt_username, "password": mqtt_password}
+        
+            publish.multiple(messages, hostname=mqtt_broker_cfg.get("host"), port=mqtt_broker_cfg.getint("port"), client_id=mqtt_broker_cfg.get("client"), auth=auth)
+        except Exception as ex:
+            print(datetime.datetime.now(), "Error publishing to MQTT: {0}".format(str(ex)))
+        
+        with open("{0}/averages.ini".format(workdir), "w") as averages_file:
+            averages.write(averages_file)
+        
+def main():
+    read_devices()
 
-        data = json.dumps({
-            "temperature": temperature,
-            "humidity": humidity,
-            "battery": battery
-        })
-
-        # Check averages
-        avg = []
-        average_count = config[device].getint("average")
-        if average_count:
-            if mac in averages.sections():
-                avg = json.loads(averages[mac]["avg"])
-
-            # Add average
-            avg.insert(0, data)
-
-            # Strip data
-            avg = avg[0:average_count]
-
-            # Calc averages
-            temperature = 0
-            humidity = 0
-            battery = 0
-
-            for a in avg:
-                al = json.loads(a)
-                temperature += al["temperature"]
-                humidity += al["humidity"]
-                battery += al["battery"]
-
-            temperature = round(temperature / len(avg), 1)
-            humidity = round(humidity / len(avg), 1)
-            battery = round(battery / len(avg), 1)
-
-            # Convert averages
-            averages[mac] = {}
-            averages[mac]["avg"] = json.dumps(avg)
-
-            # Rewrite data
-            data = json.dumps({
-                "temperature": temperature,
-                "humidity": humidity,
-                "battery": int(battery),
-                "average": len(avg)
-            })
-
-        print(datetime.datetime.now(), device, " : ", data)
-        messages.append({'topic': config[device].get("topic"), 'payload': data, 'retain': config[device].getboolean("retain", False)})
-        availability = 'online'
-    except BTLEException as e:
-        availability = 'offline'
-        print(datetime.datetime.now(), "Error connecting to device {0}: {1}".format(device, str(e)))
-    except Exception as e:
-        availability = 'offline'
-        print(datetime.datetime.now(), "Error polling device {0}. Device might be unreachable or offline.".format(device))
-        # print(traceback.print_exc())
-    finally:
-        messages.append({'topic': config[device].get("availability_topic"), 'payload': availability, 'retain': config[device].getboolean("retain", False)})
-
-
-# Init MQTT
-mqtt_config = configparser.ConfigParser()
-mqtt_config.read("{0}/mqtt.ini".format(workdir))
-mqtt_broker_cfg = mqtt_config["broker"]
-
-try:
-    auth = None
-    mqtt_username = mqtt_broker_cfg.get("username")
-    mqtt_password = mqtt_broker_cfg.get("password")
-
-    if mqtt_username:
-        auth = {"username": mqtt_username, "password": mqtt_password}
-
-    publish.multiple(messages, hostname=mqtt_broker_cfg.get("host"), port=mqtt_broker_cfg.getint("port"), client_id=mqtt_broker_cfg.get("client"), auth=auth)
-except Exception as ex:
-    print(datetime.datetime.now(), "Error publishing to MQTT: {0}".format(str(ex)))
-
-with open("{0}/averages.ini".format(workdir), "w") as averages_file:
-    averages.write(averages_file)
+if __name__ == '__main__':
+    main()
